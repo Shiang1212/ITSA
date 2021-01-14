83b6b4686843fcb377d0c2d55fac3d7ba5e964a1
Shiang1212 <64146450+Shiang1212@users.noreply.github.com>
2021-01-14 20:41:58 +0800

    Update [C_MM06-易] 英哩轉公里.c

--- 數學/[C_MM06-易] 英哩轉公里/[C_MM06-易] 英哩轉公里.c
+++ 數學/[C_MM06-易] 英哩轉公里/[C_MM06-易] 英哩轉公里.c
@@ -3,10 +3,10 @@ int main()
 {
     float a;
     double b;
-    while(scanf("%f",&a)!=EOF)
+    while(scanf("%f",&a)!=EOF)  //連續讀入a=英哩
     {
-        a=a*1.6;
-        b =(int)((a * 10) + 0.5) / 10.0;
+        a=a*1.6;    //轉公里
+        b =(int)((a * 10) + 0.5) / 10.0;    //四捨五入到小數第一位
         printf("%.1lf\n",b);
     }
     return 0;
