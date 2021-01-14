8c5c9bdf73e5d749276880b03217365a6c736b63
Shiang1212 <64146450+Shiang1212@users.noreply.github.com>
2021-01-14 20:39:25 +0800

    Update [C_MM02-易] 計算三角形面積.C

--- 數學/[C_MM02-易] 計算三角形面積/[C_MM02-易] 計算三角形面積.C
+++ 數學/[C_MM02-易] 計算三角形面積/[C_MM02-易] 計算三角形面積.C
@@ -2,9 +2,9 @@
 int main()
 {
 	float a,b;
-	while(scanf("%f%f", &a, &b)!=EOF)
+	while(scanf("%f%f", &a, &b)!=EOF)  //連續讀取a=底,b=高
     {
-	printf("%.1lf\n",a*b/2);
+	printf("%.1lf\n",a*b/2);	//a*b/2為三角形面積公式
     }
 	return 0;
 }
