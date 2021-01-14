ae5b56795e5a80a66a841622e9432ed4fbb3fded
Shiang1212 <64146450+Shiang1212@users.noreply.github.com>
2021-01-14 21:01:53 +0800

    Update [C_AR63-易] 矩陣相加.c

--- 陣列/[C_AR63-易] 矩陣相加/[C_AR63-易] 矩陣相加.c
+++ 陣列/[C_AR63-易] 矩陣相加/[C_AR63-易] 矩陣相加.c
@@ -2,26 +2,26 @@
 int main()
 {
     int a,b;
-    scanf("%d%d",&a,&b);
+    scanf("%d%d",&a,&b);    //讀入陣列大小
     int m[a*b];
     int n[a*b];
     int sum[a*b];
     for(int i=0;i<a*b;i++)
     {
-        scanf("%d",&m[i]);
+        scanf("%d",&m[i]);  //讀入陣列內容
     }
-    scanf("%d%d",&a,&b);
+    scanf("%d%d",&a,&b);    //讀入陣列大小
     for(int i=0;i<a*b;i++)
     {
-        scanf("%d",&n[i]);
+        scanf("%d",&n[i]);  //讀入陣列內容
     }
     for(int i=0;i<a*b;i++)
     {
-        sum[i]=m[i]+n[i];
+        sum[i]=m[i]+n[i];   //各項加總
     }
-    for(int i=0,z=0;i<a;i++)
+    for(int i=0,z=0;i<a;i++)    //每a項換一行
     {
-        for(int j=0;j<b;j++,z++)
+        for(int j=0;j<b;j++,z++)    //每行有b項
         {
             printf("%d",sum[z]);
             if(j<b-1)
