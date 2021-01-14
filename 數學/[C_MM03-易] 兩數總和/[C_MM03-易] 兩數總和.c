fee9543b476637d1520d40d190949245c16ae9ce
Shiang1212 <64146450+Shiang1212@users.noreply.github.com>
2021-01-14 20:40:00 +0800

    Update [C_MM03-易] 兩數總和.c

--- 數學/[C_MM03-易] 兩數總和/[C_MM03-易] 兩數總和.c
+++ 數學/[C_MM03-易] 兩數總和/[C_MM03-易] 兩數總和.c
@@ -2,7 +2,7 @@
 int main()
 {
     int a,b;
-    while(scanf("%d%d",&a,&b)!=EOF)
-    printf("%d\n",a+b);
+    while(scanf("%d%d",&a,&b)!=EOF) //連續讀入兩數
+    printf("%d\n",a+b); //總和
     return 0;
 }
