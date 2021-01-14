f5e1a7813f3278a23f13130f0cd49cfd1ae8f613
Shiang1212 <64146450+Shiang1212@users.noreply.github.com>
2021-01-14 20:59:47 +0800

    Update [C_AR03-易] 計算陣列中所有元素的立方和.c

--- 陣列/[C_AR03-易] 計算陣列中所有元素的立方和/[C_AR03-易] 計算陣列中所有元素的立方和.c
+++ 陣列/[C_AR03-易] 計算陣列中所有元素的立方和/[C_AR03-易] 計算陣列中所有元素的立方和.c
@@ -2,6 +2,6 @@
 int main()
 {
     int a,b,c,d,e,f;
-    scanf("%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f);
-    printf("%d\n",a*a*a+b*b*b+c*c*c+d*d*d+e*e*e+f*f*f);
+    scanf("%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f);    //輸入5數
+    printf("%d\n",a*a*a+b*b*b+c*c*c+d*d*d+e*e*e+f*f*f); //立方和
 }
