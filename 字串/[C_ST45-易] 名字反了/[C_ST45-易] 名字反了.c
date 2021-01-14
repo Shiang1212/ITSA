731bce6dd2492b135f449fa7815e5cc18ec6f15c
Shiang1212 <64146450+Shiang1212@users.noreply.github.com>
2021-01-14 20:33:55 +0800

    Update [C_ST45-易] 名字反了.c

--- 字串/[C_ST45-易] 名字反了/[C_ST45-易] 名字反了.c
+++ 字串/[C_ST45-易] 名字反了/[C_ST45-易] 名字反了.c
@@ -5,18 +5,18 @@ int main()
 {
     int cnt;
     char input[20];
-    scanf("%d",&cnt);
+    scanf("%d",&cnt);   //讀取字串數
     for(int m=0;m<cnt;m++)
     {
         char output[20]={0};
-        scanf("%s",input);
+        scanf("%s",input);  //讀取字串內容
         int len=strlen(input);
-        for(int i=len-1,j=0;i>=0;i--,j++)
+        for(int i=len-1,j=0;i>=0;i--,j++)   //從字串尾端讀取回來
         {
-            output[j]=input[i];
+            output[j]=input[i]; //用output[]存取迴文
         }
-        output[0]=toupper(output[0]);
-        output[len-1]=tolower(output[len-1]);
+        output[0]=toupper(output[0]);   //首字大寫
+        output[len-1]=tolower(output[len-1]);   //尾字小寫
         printf("%s\n",output);
     }
 }
