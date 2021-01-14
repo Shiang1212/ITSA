70c7766e2f3bc4f1699bf580435d17ee3646fa37
Shiang1212 <64146450+Shiang1212@users.noreply.github.com>
2021-01-14 20:37:44 +0800

    Update [C_MM01-易] 計算梯型面積.c

--- 數學/[C_MM01-易] 計算梯型面積/[C_MM01-易] 計算梯型面積.c
+++ 數學/[C_MM01-易] 計算梯型面積/[C_MM01-易] 計算梯型面積.c
@@ -2,7 +2,7 @@
 int main()
 {
     float a,b,c;
-    while(scanf("%f%f%f", &a, &b, &c) != EOF)
+    while(scanf("%f%f%f", &a, &b, &c) != EOF)   //連續讀取a=上底,b=下底,c=高
     {
         printf("Trapezoid area:%.1lf\n",(a+b)*c/2);
     }
