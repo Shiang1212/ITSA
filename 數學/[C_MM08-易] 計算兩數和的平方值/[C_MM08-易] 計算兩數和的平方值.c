2610258b58a71754b0cab481ab967119aefb59d9
Shiang1212 <64146450+Shiang1212@users.noreply.github.com>
2021-01-14 20:43:41 +0800

    Update [C_MM08-易] 計算兩數和的平方值.c

--- 數學/[C_MM08-易] 計算兩數和的平方值/[C_MM08-易] 計算兩數和的平方值.c
+++ 數學/[C_MM08-易] 計算兩數和的平方值/[C_MM08-易] 計算兩數和的平方值.c
@@ -2,9 +2,9 @@
 int main()
 {
     int a,b;
-    while(scanf("%d%d",&a,&b)!= EOF)
+    while(scanf("%d%d",&a,&b)!= EOF)    //連續讀入兩數
     {
-        printf("%d\n",(a+b)*(a+b));
+        printf("%d\n",(a+b)*(a+b)); //和,平方
     }
     return 0;
 }
