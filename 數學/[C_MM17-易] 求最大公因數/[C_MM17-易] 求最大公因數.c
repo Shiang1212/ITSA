01c81aaae206fe0eac5f213b5ba7653b6f829548
Shiang1212 <64146450+Shiang1212@users.noreply.github.com>
2021-01-14 20:51:04 +0800

    Update [C_MM17-易] 求最大公因數.c

--- 數學/[C_MM17-易] 求最大公因數/[C_MM17-易] 求最大公因數.c
+++ 數學/[C_MM17-易] 求最大公因數/[C_MM17-易] 求最大公因數.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int gcd(int m, int n)
+int gcd(int m, int n)   //最大公因數
 {
     while(n != 0)
     {
@@ -16,7 +16,7 @@ int main()
 {
     int a,b;
     int k;
-    scanf("%d%d",&a,&b);
+    scanf("%d%d",&a,&b);  //讀入兩數
     k = gcd(a,b);
     printf("%d\n",k);
 }
