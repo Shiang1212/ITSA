53feabfcf2cd4a0bb303d2ada32f63ece7ed09cc
Shiang1212 <64146450+Shiang1212@users.noreply.github.com>
2021-01-14 20:43:07 +0800

    Update [C_MM07-易] 計算平方值與立方值.c

--- 數學/[C_MM07-易] 計算平方值與立方值/[C_MM07-易] 計算平方值與立方值.c
+++ 數學/[C_MM07-易] 計算平方值與立方值/[C_MM07-易] 計算平方值與立方值.c
@@ -2,9 +2,9 @@
 int main()
 {
     int a;
-    while(scanf("%d",&a)!= EOF)
+    while(scanf("%d",&a)!= EOF) //連續讀入數值
     {
-        printf("%d %d %d\n",a,a*a,a*a*a);
+        printf("%d %d %d\n",a,a*a,a*a*a);   //a,a的平方,a的立方
     }
     return 0;
 }
