9b74f0ae9ceda8f5d14e2d253460d50b11c9bf71
Shiang1212 <64146450+Shiang1212@users.noreply.github.com>
2021-01-14 20:57:09 +0800

    Update [C_AR02-易] 一維陣列反轉 II.c

--- 陣列/[C_AR02-易] 一維陣列反轉 II/[C_AR02-易] 一維陣列反轉 II.c
+++ 陣列/[C_AR02-易] 一維陣列反轉 II/[C_AR02-易] 一維陣列反轉 II.c
@@ -5,14 +5,14 @@ int main()
     int i=0;
     for(i=0;i<6;i++)
     {
-        scanf("%d",&num[i]);
+        scanf("%d",&num[i]);    //連續讀入陣列內容
     }
-    for(i=i-1;i>=0;i--)
+    for(i=i-1;i>=0;i--)  //執行n次                      
     {
         printf("%d",num[i]);
         if(i>0)
         {
-            printf(" ");
+            printf(" ");    //輸出n-1次
         }
     }
     printf("\n");
