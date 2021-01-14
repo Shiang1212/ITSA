5c4fa0f52f36145114e75dd1e44794aa11aaf5f5
Shiang1212 <64146450+Shiang1212@users.noreply.github.com>
2021-01-14 20:44:24 +0800

    Update [C_MM09-易] 計算 i 次方的值.c

--- 數學/[C_MM09-易] 計算 i 次方的值/[C_MM09-易] 計算 i 次方的值.c
+++ 數學/[C_MM09-易] 計算 i 次方的值/[C_MM09-易] 計算 i 次方的值.c
@@ -3,14 +3,14 @@
 int main()
 {
     int a,i;
-    while(scanf("%d",&i)!=EOF)
+    while(scanf("%d",&i)!=EOF)  //連續讀入i
     {
         if(i>=31)
         {
             printf("Value of more than 31\n");
             continue;
         }
-        a=1<<i;
+        a=1<<i; //a=pow(2,i)
         printf("%d\n",a);
     }
     return 0;
