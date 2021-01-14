0ec132ed8c4da0699f7dcca741b1f7a3394c5601
Shiang1212 <64146450+Shiang1212@users.noreply.github.com>
2021-01-14 20:45:45 +0800

    Update [C_MM11-易] 購票計算.c

--- 數學/[C_MM11-易] 購票計算/[C_MM11-易] 購票計算.c
+++ 數學/[C_MM11-易] 購票計算/[C_MM11-易] 購票計算.c
@@ -2,10 +2,10 @@
 int main()
 {
     int m,a,b,c;
-    scanf("%d",&m);
-    a=m/10;
-    b=(m%10)/5;
-    c=(m%10)%5;
+    scanf("%d",&m); //m=金額
+    a=m/10;     //a=10元數
+    b=(m%10)/5; //b=5元數
+    c=(m%10)%5; //c=1元數
     printf("NT10=%d\n",a);
     printf("NT5=%d\n",b);
     printf("NT1=%d\n",c);
