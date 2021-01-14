1e21c9c5d1d771f5803e7661133c11a113d64d80
Shiang1212 <64146450+Shiang1212@users.noreply.github.com>
2021-01-14 20:27:57 +0800

    Update [C_ST02-易] 迴文字串.c

--- 字串/[C_ST02-易] 迴文字串/[C_ST02-易] 迴文字串.c
+++ 字串/[C_ST02-易] 迴文字串/[C_ST02-易] 迴文字串.c
@@ -4,11 +4,11 @@ int main()
 {
     int cnt;
     char str[20];
-    scanf("%d",&cnt);
+    scanf("%d",&cnt);   //輸入字串數
     for(int i=0;i<cnt;i++)
     {
-        scanf("%s",str);
-        for(int j=strlen(str)-1;j>=0;j--)
+        scanf("%s",str);    //輸入字串內容
+        for(int j=strlen(str)-1;j>=0;j--)   //從字串尾端讀回來
         {
             printf("%c",str[j]);
         }
