fdcf08f7dd327d3fbb482eb22c78cbe823d6bc03
Shiang1212 <64146450+Shiang1212@users.noreply.github.com>
2021-01-14 20:41:01 +0800

    Update [C_MM05-易] 計算正方形面積.c

--- 數學/[C_MM05-易] 計算正方形面積/[C_MM05-易] 計算正方形面積.c
+++ 數學/[C_MM05-易] 計算正方形面積/[C_MM05-易] 計算正方形面積.c
@@ -2,10 +2,10 @@
 int main()
 {
     float a,b;
-    while(scanf("%f",&a)!=EOF)
+    while(scanf("%f",&a)!=EOF)  //連續讀入a=正方形邊長
     {
         a=a*a;
-        b =(int)((a * 10) + 0.5) / 10.0;
+        b =(int)((a * 10) + 0.5) / 10.0;    //四捨五入到小數第一位
         printf("%.1lf",b);
     }
     return 0;
