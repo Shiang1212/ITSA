1cb68b4595c81e5e8a0aaf15cdc9915e570a6661
Shiang1212 <64146450+Shiang1212@users.noreply.github.com>
2021-01-14 20:49:44 +0800

    Update [C_MM12-易] 相遇時間計算.c

--- 數學/[C_MM12-易] 相遇時間計算/[C_MM12-易] 相遇時間計算.c
+++ 數學/[C_MM12-易] 相遇時間計算/[C_MM12-易] 相遇時間計算.c
@@ -2,13 +2,13 @@
 int main()
 {
     int n,j;
-    while(scanf("%d",&n)!=EOF)
+    while(scanf("%d",&n)!=EOF)	//連續讀入n
     {
-        if(n*1000%238==0)
+        if(n*1000%238==0)  //整除
         {
-            printf("%d\n",n*100/23.8);
+            printf("%d\n",n*100/23.8);	//公尺轉英吋
         }
-        else
+        else	//無條件進入
 		{
 			 j=n*100/23.8;
 			 printf("%d\n",j+1);
