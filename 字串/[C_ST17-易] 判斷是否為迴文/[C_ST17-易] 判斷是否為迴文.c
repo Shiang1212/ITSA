c1ff9b7719c5a32dc58ae12918f0f23ffc28f116
Shiang1212 <64146450+Shiang1212@users.noreply.github.com>
2021-01-14 20:30:19 +0800

    Update [C_ST17-易] 判斷是否為迴文.c

--- 字串/[C_ST17-易] 判斷是否為迴文/[C_ST17-易] 判斷是否為迴文.c
+++ 字串/[C_ST17-易] 判斷是否為迴文/[C_ST17-易] 判斷是否為迴文.c
@@ -4,13 +4,13 @@ int main()
 {
   int i,l,j;
   char input[1000];
-  while(scanf("%s",input)!=EOF)
+  while(scanf("%s",input)!=EOF) //連續輸入字串內容
   {
       char rev[1000]={0};
       l=strlen(input);
-      for(i=l-1,j=0;i>=0;i--,j++)
+      for(i=l-1,j=0;i>=0;i--,j++) //從後面讀取回來
       {
-          rev[j]=input[i];
+          rev[j]=input[i];  //用rev[]存取迴文
       }
     if(strcmp(input,rev)==0)
     {
